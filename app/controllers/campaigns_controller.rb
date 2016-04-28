db829a9fc8f6ec0db1140989bbcfaca6cc52e7ee
Zachary Patten <zpatten@jovelabs.io>
2016-04-28 11:53:51 -0700

    add ordered_campaign view

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,9 +1,25 @@
 class CampaignsController < ApplicationController
 
   def ordered_campaigns
-    @campaigns = Campaign.order(:id).all
+    campaigns = Campaign.includes(:campaign_qualifications).order("campaigns.cpi DESC, campaigns.length_of_interview ASC").all
 
-    render :json => @campaigns
+    @ordered_questions = Hash.new { |hash,key| hash[key] = Array.new }
+
+    campaigns.each do |campaign|
+      if campaign.campaign_quotas.empty?
+        @ordered_questions["Empty"] << campaign.name
+        next
+      end
+
+      campaign.campaign_qualifications.each do |campaign_qualification|
+        if (campaign_qualification.question_id == 43 && campaign_qualification.pre_codes.split(",").include?("2"))
+          campaign_name = "<b>#{campaign.name}</b>"
+        else
+          campaign_name = campaign.name
+        end
+        @ordered_questions["List #{campaign_qualification.question_id}"] << campaign_name
+      end
+    end
   end
 
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,4 @@
 class Campaign < ActiveRecord::Base
   has_many :campaign_quotas
+  has_many :campaign_qualifications, :through => :campaign_quotas
 end
